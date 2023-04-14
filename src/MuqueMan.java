9d37e04cbb2482f7765afb4c143e18ab89a9d3d1
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-04-14 07:01:27 -0300

    golpes do muque man funcionando

--- src/MuqueMan.java
+++ src/MuqueMan.java
@@ -11,8 +11,29 @@ public class MuqueMan extends Personagem implements Atacavel {
         System.out.println("Muque Man usou o chute!");
 
     }
+    
+    @Override
+    
+    public void golpearMedio(Personagem alvo){
+        int pontosDeVidaAtuais = alvo.getPontosDeVida();
 
+        pontosDeVidaAtuais = pontosDeVidaAtuais -20;
 
+        alvo.setPontosDeVida(pontosDeVidaAtuais);
+        System.out.println("Muque man usou o Tornado!");
+    }
+    
+    @Override
+    
+    public void golpearForte(Personagem alvo){
+
+        int pontosDeVidaAtuais = alvo.getPontosDeVida();
+
+        pontosDeVidaAtuais = pontosDeVidaAtuais -40;
+
+        alvo.setPontosDeVida(pontosDeVidaAtuais);
+        System.out.println("Muque man usou o Berserker!");
+    }
 
 
 
