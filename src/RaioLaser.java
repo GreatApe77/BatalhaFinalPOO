e5b566aa1f90c6fdcbbafe44a95adf66677b7720
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-04-14 07:30:54 -0300

    implementacao dos golpes dos personagens RaioLAser e Relampago

--- src/RaioLaser.java
+++ src/RaioLaser.java
@@ -1,12 +1,41 @@
-public class RaioLaser extends Personagem {
+public class RaioLaser extends Personagem implements Atacavel {
     
 
 
-    public void opticBlast(Personagem alvo){
-        this.golpearFraco(alvo);
-        System.out.println("Muque Man usou o Chute");
-    }
+    @Override
+    public void golpearFraco(Personagem alvo){
+        
+        int pontosDeVidaAtuais = alvo.getPontosDeVida();
 
+        pontosDeVidaAtuais = pontosDeVidaAtuais -10;
+        alvo.setPontosDeVida(pontosDeVidaAtuais);
+        System.out.println("Raio-Laser usou Optic Blast");
+
+    }
+    
+    @Override
+    
+    public void golpearMedio(Personagem alvo){
+        int pontosDeVidaAtuais = alvo.getPontosDeVida();
+
+        pontosDeVidaAtuais = pontosDeVidaAtuais -20;
+
+        alvo.setPontosDeVida(pontosDeVidaAtuais);
+        System.out.println("Raio-Laser usou Mega Optic");
+    }
+    
+    @Override
+    
+    public void golpearForte(Personagem alvo){
+
+        int pontosDeVidaAtuais = alvo.getPontosDeVida();
+
+        pontosDeVidaAtuais = pontosDeVidaAtuais -40;
+
+        alvo.setPontosDeVida(pontosDeVidaAtuais);
+        System.out.println("Raio-Laser usou Super Optic");
+
+    }
 
 
 }
--- src/Relampago.java
+++ src/Relampago.java
@@ -1,3 +1,41 @@
-public class Relampago {
+public class Relampago extends Personagem implements Atacavel {
     
+
+
+    
+    @Override
+    public void golpearFraco(Personagem alvo){
+        
+        int pontosDeVidaAtuais = alvo.getPontosDeVida();
+
+        pontosDeVidaAtuais = pontosDeVidaAtuais -10;
+        alvo.setPontosDeVida(pontosDeVidaAtuais);
+        System.out.println("Relâmpago usou Lightning Attack");
+
+    }
+    
+    @Override
+    
+    public void golpearMedio(Personagem alvo){
+        int pontosDeVidaAtuais = alvo.getPontosDeVida();
+
+        pontosDeVidaAtuais = pontosDeVidaAtuais -20;
+
+        alvo.setPontosDeVida(pontosDeVidaAtuais);
+        System.out.println("Relâmpago usou Lightning Storm");
+    }
+    
+    @Override
+    
+    public void golpearForte(Personagem alvo){
+
+        int pontosDeVidaAtuais = alvo.getPontosDeVida();
+
+        pontosDeVidaAtuais = pontosDeVidaAtuais -40;
+
+        alvo.setPontosDeVida(pontosDeVidaAtuais);
+        System.out.println("Relâmpago usou Hail Storm");
+
+    }
+
 }
