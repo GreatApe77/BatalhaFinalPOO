8bb9bd8ea729036c73dd8512f7716ae3d2fc0d22
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-04-16 19:23:45 -0300

    override nos metodos do alienigena especificiando sua acoes

--- src/Alienigena.java
+++ src/Alienigena.java
@@ -1,3 +1,38 @@
-public class Alienigena extends Personagem {
+public class Alienigena extends Personagem  implements Atacavel{
     
+    //Rival Alienígena: Split Kick, Bit Laser, Shockwave
+    @Override
+    public void golpearFraco(Personagem alvo){
+        
+        int pontosDeVidaAtuais = alvo.getPontosDeVida();
+
+        pontosDeVidaAtuais = pontosDeVidaAtuais -10;
+        alvo.setPontosDeVida(pontosDeVidaAtuais);
+        System.out.println("Alienigena usou o Split Kick!");
+
+    }
+    
+    @Override
+    
+    public void golpearMedio(Personagem alvo){
+        int pontosDeVidaAtuais = alvo.getPontosDeVida();
+
+        pontosDeVidaAtuais = pontosDeVidaAtuais -20;
+
+        alvo.setPontosDeVida(pontosDeVidaAtuais);
+        System.out.println("Alienígena usou o  Bit Laser!");
+    }
+    
+    @Override
+    
+    public void golpearForte(Personagem alvo){
+
+        int pontosDeVidaAtuais = alvo.getPontosDeVida();
+
+        pontosDeVidaAtuais = pontosDeVidaAtuais -40;
+
+        alvo.setPontosDeVida(pontosDeVidaAtuais);
+        System.out.println("Alienígena usou o Shockwave!");
+    }
+
 }
