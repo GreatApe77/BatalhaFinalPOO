35e2ba58b14bd4cdb33bb9de63f38d2aa1a38bb3
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-04-20 16:56:06 -0300

    construindo a escolha de personagem

--- src/App.java
+++ src/App.java
@@ -3,7 +3,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
+        ElementosJogo elementosJogo = new ElementosJogo();
 
+        elementosJogo.iniciarJogo(0, "");
         
     }
 }
--- src/ElementosJogo.java
+++ src/ElementosJogo.java
@@ -1,28 +1,42 @@
 public class ElementosJogo {
-    public static int jogarDado(){
-        int jogadaDado; 
+
+    public static int jogarDado() {
+        int jogadaDado;
         do {
-            jogadaDado =  (int)(Math.random()*10);
-        } while (jogadaDado<1||  jogadaDado>6);
+            jogadaDado = (int) (Math.random() * 10);
+        } while (jogadaDado < 1 || jogadaDado > 6);
         return jogadaDado;
     }
 
-    public void iniciarJogo(int NumeroDeRodadas,String personagemEscolhido){
-        /*
-          
+    public void iniciarJogo(int NumeroDeRodadas, String personagemEscolhido) {
+
+        Personagem heroi = null;
+
         switch (personagemEscolhido) {
-            case valu:
-                
+            case "Muque-Man":
+                 heroi = new MuqueMan();
+                System.out.println("Escolheu Muque-Man");
                 break;
-        
+            case "Relampago":
+                 heroi = new Relampago();
+                System.out.println("Escolheu Relampago");
+                break;
+            case "Raio-Laser":
+                 heroi = new RaioLaser();
+                System.out.println("Escolheu Raio-Laser");
+                break;
             default:
-                break;
+                return;
         }
-          
-         
-         */
+        
+        
+         System.out.println("You Choose"+heroi.getNome());                       
         
 
-    }    
+
+
+
+        
+    }
 
 }
